71478ce2879049e32b66d2cf61921f90a11d8254
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-05 18:58:53 +0300

    16. Styling React Native Apps

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text</Text>
+        <Text style={styles.dummyText}>Another piece of text</Text>
       </View>
-      <Text>Hello World!</Text>
+      <Text style={styles.dummyText}>Hello World!</Text>
       <Button title="Tap me!" onPress={() => alert('Button pressed')} />
     </View>
   );
@@ -18,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
   },
 });
