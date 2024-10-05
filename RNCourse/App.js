f6509a8229ded367b45e5f8e93272b250cc0a9b4
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-05 18:08:50 +0300

    15. Working With Core Components

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another piece of text</Text>
+      </View>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Button title="Tap me!" onPress={() => alert('Button pressed')} />
     </View>
   );
 }
