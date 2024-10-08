cd589f869016befe311c8ffe748353887d9b87aa
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-09 00:45:25 +0300

    53. Adding Visual Feedback to the Buttons

--- guess-my-number/components/PrimaryButton.js
+++ guess-my-number/components/PrimaryButton.js
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('Button pressed');
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
