646fb19dc68d0950418c6cb5808f316522b4bdc6
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-10 00:37:04 +0300

    64. Managing Colors Globally

--- guess-my-number/App.js
+++ guess-my-number/App.js
@@ -10,6 +10,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
@@ -25,7 +26,10 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}
+    >
       <ImageBackground
         source={require('./assets/images/background.png')}
         resizeMode="cover"
