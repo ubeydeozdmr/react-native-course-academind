2980bd3ec74267b7dcaaa199fe2f79feb5a0933e
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-10 22:49:40 +0300

    73. Using & Styling Nested Text

--- guess-my-number/screens/GameOverScreen.js
+++ guess-my-number/screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { Image, View, Text, StyleSheet } from 'react-native';
 
 import Title from '../components/ui/Title';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
 
 function GameOverScreen() {
@@ -13,7 +14,11 @@ function GameOverScreen() {
           source={require('../assets/images/success.png')}
         />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -39,5 +44,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   },
 });
