b2f655a5590815b7e2bcbc50537f7e9bf52aecc9
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-10 19:03:40 +0300

    67. Checking for "Game Over"

---
+++ guess-my-number/screens/GameOverScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function GameOverScreen() {
+  return (
+    <View>
+      <Text>Game Over!</Text>
+    </View>
+  );
+}
+
+export default GameOverScreen;
