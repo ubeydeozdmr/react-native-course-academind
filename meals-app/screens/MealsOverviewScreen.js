d64f6ab794aa8bfdf9983433619040066136bbed
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-11 23:45:23 +0300

    100. Displaying Meals

--- meals-app/screens/MealsOverviewScreen.js
+++ meals-app/screens/MealsOverviewScreen.js
@@ -1,13 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
 
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0,
+  );
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
