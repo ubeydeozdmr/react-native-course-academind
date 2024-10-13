596fed51491a550ba9e14708ed8fafc582d53ee8
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-13 20:54:47 +0300

    136. Adding a Header Button & Making Expense Items Tappable

--- expense-tracker-app/components/ExpensesOutput/ExpenseItem.js
+++ expense-tracker-app/components/ExpensesOutput/ExpenseItem.js
@@ -4,8 +4,15 @@ import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
 function ExpenseItem({ description, amount, date }) {
+  function expensePressHandler() {
+    console.log('Expense pressed');
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -26,6 +33,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
