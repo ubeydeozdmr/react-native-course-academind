636eea529d0f605a5804bb326f5f8ca3b4c7c19d
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-13 23:03:05 +0300

    144. Using Context From Inside Components

--- expense-tracker-app/store/expenses-context.js
+++ expense-tracker-app/store/expenses-context.js
@@ -109,7 +109,18 @@ function ExpensesContextProvider({ children }) {
     dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
   }
 
-  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const value = {
+    expenses: expensesState,
+    addExpense,
+    deleteExpense,
+    updateExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 
 export default ExpensesContextProvider;
