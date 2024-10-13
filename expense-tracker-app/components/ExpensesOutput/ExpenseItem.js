3186f1a0f2170d1b0d408079dd8b1620051c2b40
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-13 21:02:21 +0300

    137. Navigating Programmatically Between Screens

--- expense-tracker-app/components/ExpensesOutput/ExpenseItem.js
+++ expense-tracker-app/components/ExpensesOutput/ExpenseItem.js
@@ -1,11 +1,14 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
 function ExpenseItem({ description, amount, date }) {
+  const navigation = useNavigation();
+
   function expensePressHandler() {
-    console.log('Expense pressed');
+    navigation.navigate('ManageExpense');
   }
 
   return (
