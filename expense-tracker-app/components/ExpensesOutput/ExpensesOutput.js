a0c413df5a9b662a264f6ecbb55943b33ec35e76
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-13 17:49:17 +0300

    131a. Adding Dummy Expense Data

--- expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
+++ expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -5,31 +5,31 @@ import ExpensesSummary from './ExpensesSummary';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'A pair of shoes',
+    description: 'A pair of shoes',
     amount: 59.99,
     date: new Date('2021-12-19'),
   },
   {
     id: 'e2',
-    title: 'A pair of trousers',
+    description: 'A pair of trousers',
     amount: 89.29,
     date: new Date('2022-01-05'),
   },
   {
     id: 'e3',
-    title: 'Some bananas',
+    description: 'Some bananas',
     amount: 5.99,
     date: new Date('2021-12-01'),
   },
   {
     id: 'e4',
-    title: 'A book',
+    description: 'A book',
     amount: 14.99,
     date: new Date('2022-02-19'),
   },
   {
     id: 'e5',
-    title: 'Another book',
+    description: 'Another book',
     amount: 18.59,
     date: new Date('2022-02-18'),
   },
