465faadbce5610bfa3b259110eb2347a6db6eb00
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-14 23:43:34 +0300

    163. Installing Axios

--- expense-tracker-app/screens/ManageExpense.js
+++ expense-tracker-app/screens/ManageExpense.js
@@ -12,6 +12,7 @@ function ManageExpense({ route, navigation }) {
 
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+  ('');
 
   const selectedExpense = expensesCtx.expenses.find(
     (expense) => expense.id === editedExpenseId,
