50e9f275a8f1adc0ff6a37b5babfa5df696d6032
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-19 13:30:44 +0300

    218. Adding the Place Details Screen

--- favorite-places-app/App.js
+++ favorite-places-app/App.js
@@ -10,6 +10,7 @@ import IconButton from './components/UI/IconButton';
 import { Colors } from './constants/colors';
 import Map from './screens/Map';
 import { init } from './util/database';
+import PlaceDetails from './screens/PlaceDetails';
 
 const Stack = createNativeStackNavigator();
 
@@ -64,6 +65,7 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="PlaceDetails" component={PlaceDetails} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
