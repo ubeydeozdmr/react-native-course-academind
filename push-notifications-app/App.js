6b9d18ddfd4355656621771fd829da8cfd1e7214
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-20 13:26:44 +0300

    246. Scheduling Notifications

--- push-notifications-app/App.js
+++ push-notifications-app/App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification.',
+        data: { userName: 'ubeydeozdmr' },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
