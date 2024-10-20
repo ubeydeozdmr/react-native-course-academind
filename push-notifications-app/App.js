610467abe382bdfe90618ad4328aaeb5dde594c1
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-10-20 18:20:07 +0300

    248. Handling Incoming Notifications

--- push-notifications-app/App.js
+++ push-notifications-app/App.js
@@ -2,8 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
+
+const allowsNotificationsAsync = async () => {
+  const settings = await Notifications.getPermissionsAsync();
+  return (
+    settings.granted ||
+    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  );
+};
+
+const requestPermissionsAsync = async () => {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  });
+};
+
 export default function App() {
-  function scheduleNotificationHandler() {
+  const scheduleNotificationHandler = async () => {
+    const hasPushNotificationPermissionGranted =
+      await allowsNotificationsAsync();
+
+    if (!hasPushNotificationPermissionGranted) {
+      await requestPermissionsAsync();
+    }
+
     Notifications.scheduleNotificationAsync({
       content: {
         title: 'My first local notification',
@@ -14,7 +50,7 @@ export default function App() {
         seconds: 5,
       },
     });
-  }
+  };
 
   return (
     <View style={styles.container}>
